38b0e393488f0b3bc19724087ac8e2bd8ead4425
7657137 <7657137@student.swin.edu.au>
2016-10-18 21:55:30 +1100

    Adding powerups abstract class

---
+++ BattleShips/src/Model/PowerUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SwinGameSDK;
+
+namespace MyGame
+{
+	public abstract class PowerUp
+	{
+	
+		private string name;
+		private Color displayColour;
+		private SoundEffect activationSound;
+		private SoundEffect music;
+		private Bitmap icon;
+
+
+		public PowerUp ()
+		{
+		}
+	}
+}
+
